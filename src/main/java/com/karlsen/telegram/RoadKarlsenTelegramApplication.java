9c0e1cbcc855851e987925e53315bf2fdd292a28
ming.karlsen <ming.karlsen@gmail.com>
2020-03-17 15:47:23 +0800

    init repo

---
+++ src/main/java/com/karlsen/telegram/RoadKarlsenTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.karlsen.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadKarlsenTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadKarlsenTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karlsen/telegram/RoadKarlsenTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlsen.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadKarlsenTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
